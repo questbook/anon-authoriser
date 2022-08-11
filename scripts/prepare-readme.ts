601c213ffc0f5f6ea1968e53f6e66d922881e44e
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-08-11 19:41:21 +0530

    fix: readme prep script

--- scripts/prepare-readme.ts
+++ scripts/prepare-readme.ts
@@ -1,19 +1,21 @@
 import { readFile, writeFile } from 'fs/promises'
 import CONTRACT_ADDRESS_MAP from '../src/contract-address-map.json'
 
+const README_PATH = './README.md'
+
 async function main() {
-	let readme = await readFile('../README.md', { encoding: 'utf-8' })
+	let readme = await readFile(README_PATH, { encoding: 'utf-8' })
 	const addressList = (Object.keys(CONTRACT_ADDRESS_MAP) as (keyof typeof CONTRACT_ADDRESS_MAP)[])
 		.map(key => `${key}: ${CONTRACT_ADDRESS_MAP[key].address}`)
 		.join('\n')
-	const REGEX = /following chains:\n+```\n(.+)\n```/gm
+	const REGEX = /following chains:\n+```\n([a-z:\-0-9\s]+)\n```/gim
 	const match = REGEX.exec(readme)
 	if(!match) {
 		throw new Error('could not find address list in readme')
 	}
 
 	readme = readme.replace(match[1], addressList)
-	await writeFile('../README.md', readme)
+	await writeFile(README_PATH, readme)
 }
 
 main()
